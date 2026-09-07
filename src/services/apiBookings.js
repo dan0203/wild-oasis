15830da4705697a8ce170bafbcfc99aa31a1547c
Dan Zerbib <danzerbib@gmail.com>
2026-09-07 15:26:12 +0200

    feat: add Filter to bookings
    - use BookingTableOperations component in Bookings
    - rewrite apiBookings/getBookings func for dynamic filtering for reusability
    - get filter values in useBookings hook from url with SearchParams and add filter to useQuery's dependency array and getBookings's param

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,8 +1,13 @@
 import { getToday } from '../utils/helpers';
 import supabase from './supabase';
 
-export async function getBookings() {
-    const { data, error } = await supabase.from('bookings').select('id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)');
+export async function getBookings({ filter }) {
+    let query = supabase.from('bookings').select('id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)');
+
+    // FILTER
+    if (filter !== null) query = query[filter.method || 'eq'](filter.field, filter.value);
+
+    const { data, error } = await query;
 
     if (error) {
         console.error(error);
