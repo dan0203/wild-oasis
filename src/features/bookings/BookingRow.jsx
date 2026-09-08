4f0201919767d520c2891d7e6c61eb4fb21b6692
Dan Zerbib <danzerbib@gmail.com>
2026-09-08 17:21:46 +0200

    feat: checking out a booking

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -10,7 +10,8 @@ import { formatCurrency } from '../../utils/helpers';
 import { formatDistanceFromNow } from '../../utils/helpers';
 import { HiEye } from 'react-icons/hi';
 import { useNavigate } from 'react-router-dom';
-import { HiArrowDownOnSquare } from 'react-icons/hi2';
+import { HiArrowDownOnSquare, HiArrowUpOnSquare } from 'react-icons/hi2';
+import { useCheckout } from '../check-in-out/useCheckout';
 
 const Cabin = styled.div`
     font-size: 1.6rem;
@@ -54,6 +55,7 @@ function BookingRow({
     },
 }) {
     const navigate = useNavigate();
+    const { checkout, isCheckingOut } = useCheckout();
 
     const statusToTagName = {
         unconfirmed: 'blue',
@@ -93,6 +95,12 @@ function BookingRow({
                     {status === 'unconfirmed' && (
                         <Menus.Button icon={<HiArrowDownOnSquare />} onClick={() => navigate(`/checkin/${bookingId}`)}>
                             Check in
+                        </Menus.Button>
+                    )}
+
+                    {status === 'checked-in' && (
+                        <Menus.Button icon={<HiArrowUpOnSquare />} onClick={() => checkout(bookingId)} disabled={isCheckingOut}>
+                            Check out
                         </Menus.Button>
                     )}
                 </Menus.List>
